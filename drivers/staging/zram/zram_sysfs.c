7e5fcd9da37bf8a66bb530e284728506c7b8f196
Maxime Bergeron <Maxime@MacBook-Pro-de-Maxime.local>
2013-01-30 12:32:10 -0500

    Updated zRam (faux123) and defconfig

--- drivers/staging/zram/zram_sysfs.c
+++ drivers/staging/zram/zram_sysfs.c
@@ -21,11 +21,11 @@
 static u64 zram_stat64_read(struct zram *zram, u64 *v)
 {
 	u64 val;
-
+    
 	spin_lock(&zram->stat64_lock);
 	val = *v;
 	spin_unlock(&zram->stat64_lock);
-
+    
 	return val;
 }
 
@@ -33,163 +33,169 @@ static struct zram *dev_to_zram(struct device *dev)
 {
 	int i;
 	struct zram *zram = NULL;
-
-	for (i = 0; i < num_devices; i++) {
+    
+	for (i = 0; i < zram_num_devices; i++) {
 		zram = &zram_devices[i];
 		if (disk_to_dev(zram->disk) == dev)
 			break;
 	}
-
+    
 	return zram;
 }
 
 static ssize_t disksize_show(struct device *dev,
-		struct device_attribute *attr, char *buf)
+                             struct device_attribute *attr, char *buf)
 {
 	struct zram *zram = dev_to_zram(dev);
-
+    
 	return sprintf(buf, "%llu\n", zram->disksize);
 }
 
 static ssize_t disksize_store(struct device *dev,
-		struct device_attribute *attr, const char *buf, size_t len)
+                              struct device_attribute *attr, const char *buf, size_t len)
 {
 	int ret;
+	u64 disksize;
 	struct zram *zram = dev_to_zram(dev);
-
+    
+	ret = strict_strtoull(buf, 10, &disksize);
+	if (ret)
+		return ret;
+    
+	down_write(&zram->init_lock);
 	if (zram->init_done) {
+		up_write(&zram->init_lock);
 		pr_info("Cannot change disksize for initialized device\n");
 		return -EBUSY;
 	}
-
-	ret = strict_strtoull(buf, 10, &zram->disksize);
-	if (ret)
-		return ret;
-
-	zram->disksize = PAGE_ALIGN(zram->disksize);
+    
+	zram->disksize = PAGE_ALIGN(disksize);
 	set_capacity(zram->disk, zram->disksize >> SECTOR_SHIFT);
-
+	up_write(&zram->init_lock);
+    
 	return len;
 }
 
 static ssize_t initstate_show(struct device *dev,
-		struct device_attribute *attr, char *buf)
+                              struct device_attribute *attr, char *buf)
 {
 	struct zram *zram = dev_to_zram(dev);
-
+    
 	return sprintf(buf, "%u\n", zram->init_done);
 }
 
 static ssize_t reset_store(struct device *dev,
-		struct device_attribute *attr, const char *buf, size_t len)
+                           struct device_attribute *attr, const char *buf, size_t len)
 {
 	int ret;
 	unsigned long do_reset;
 	struct zram *zram;
 	struct block_device *bdev;
-
+    
 	zram = dev_to_zram(dev);
 	bdev = bdget_disk(zram->disk, 0);
-
+    
 	/* Do not reset an active device! */
 	if (bdev->bd_holders)
 		return -EBUSY;
-
+    
 	ret = strict_strtoul(buf, 10, &do_reset);
 	if (ret)
 		return ret;
-
+    
 	if (!do_reset)
 		return -EINVAL;
-
+    
 	/* Make sure all pending I/O is finished */
 	if (bdev)
 		fsync_bdev(bdev);
-
+    
+	down_write(&zram->init_lock);
 	if (zram->init_done)
-		zram_reset_device(zram);
-
+		__zram_reset_device(zram);
+	up_write(&zram->init_lock);
+    
 	return len;
 }
 
 static ssize_t num_reads_show(struct device *dev,
-		struct device_attribute *attr, char *buf)
+                              struct device_attribute *attr, char *buf)
 {
 	struct zram *zram = dev_to_zram(dev);
-
+    
 	return sprintf(buf, "%llu\n",
-		zram_stat64_read(zram, &zram->stats.num_reads));
+                   zram_stat64_read(zram, &zram->stats.num_reads));
 }
 
 static ssize_t num_writes_show(struct device *dev,
-		struct device_attribute *attr, char *buf)
+                               struct device_attribute *attr, char *buf)
 {
 	struct zram *zram = dev_to_zram(dev);
-
+    
 	return sprintf(buf, "%llu\n",
-		zram_stat64_read(zram, &zram->stats.num_writes));
+                   zram_stat64_read(zram, &zram->stats.num_writes));
 }
 
 static ssize_t invalid_io_show(struct device *dev,
-		struct device_attribute *attr, char *buf)
+                               struct device_attribute *attr, char *buf)
 {
 	struct zram *zram = dev_to_zram(dev);
-
+    
 	return sprintf(buf, "%llu\n",
-		zram_stat64_read(zram, &zram->stats.invalid_io));
+                   zram_stat64_read(zram, &zram->stats.invalid_io));
 }
 
 static ssize_t notify_free_show(struct device *dev,
-		struct device_attribute *attr, char *buf)
+                                struct device_attribute *attr, char *buf)
 {
 	struct zram *zram = dev_to_zram(dev);
-
+    
 	return sprintf(buf, "%llu\n",
-		zram_stat64_read(zram, &zram->stats.notify_free));
+                   zram_stat64_read(zram, &zram->stats.notify_free));
 }
 
 static ssize_t zero_pages_show(struct device *dev,
-		struct device_attribute *attr, char *buf)
+                               struct device_attribute *attr, char *buf)
 {
 	struct zram *zram = dev_to_zram(dev);
-
+    
 	return sprintf(buf, "%u\n", zram->stats.pages_zero);
 }
 
 static ssize_t orig_data_size_show(struct device *dev,
-		struct device_attribute *attr, char *buf)
+                                   struct device_attribute *attr, char *buf)
 {
 	struct zram *zram = dev_to_zram(dev);
-
+    
 	return sprintf(buf, "%llu\n",
-		(u64)(zram->stats.pages_stored) << PAGE_SHIFT);
+                   (u64)(zram->stats.pages_stored) << PAGE_SHIFT);
 }
 
 static ssize_t compr_data_size_show(struct device *dev,
-		struct device_attribute *attr, char *buf)
+                                    struct device_attribute *attr, char *buf)
 {
 	struct zram *zram = dev_to_zram(dev);
-
+    
 	return sprintf(buf, "%llu\n",
-		zram_stat64_read(zram, &zram->stats.compr_size));
+                   zram_stat64_read(zram, &zram->stats.compr_size));
 }
 
 static ssize_t mem_used_total_show(struct device *dev,
-		struct device_attribute *attr, char *buf)
+                                   struct device_attribute *attr, char *buf)
 {
 	u64 val = 0;
 	struct zram *zram = dev_to_zram(dev);
-
+    
 	if (zram->init_done) {
 		val = xv_get_total_size_bytes(zram->mem_pool) +
-			((u64)(zram->stats.pages_expand) << PAGE_SHIFT);
+        ((u64)(zram->stats.pages_expand) << PAGE_SHIFT);
 	}
-
+    
 	return sprintf(buf, "%llu\n", val);
 }
 
 static DEVICE_ATTR(disksize, S_IRUGO | S_IWUSR,
-		disksize_show, disksize_store);
+                   disksize_show, disksize_store);
 static DEVICE_ATTR(initstate, S_IRUGO, initstate_show, NULL);
 static DEVICE_ATTR(reset, S_IWUSR, NULL, reset_store);
 static DEVICE_ATTR(num_reads, S_IRUGO, num_reads_show, NULL);
